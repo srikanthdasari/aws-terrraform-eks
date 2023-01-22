771d694b8cb4ff629f376057ac497200e813074d
dasari <srikanth.dasari@outlook.com>
2023-01-22 01:59:48 +0000

    version changes

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,8 +1,8 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "17.24.0"
+  version         = "19.5.1"
   cluster_name    = local.cluster_name
-  cluster_version = "1.20"
+  cluster_version = "1.24"
   subnets         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
