80a4e451722c4be188a97eb159423ac5ffb7f0eb
dasari <srikanth.dasari@outlook.com>
2023-05-14 00:24:12 -0400

    bit a change

--- s3.tf
+++ s3.tf
@@ -44,6 +44,23 @@ resource "aws_iam_policy" "s3_access_policy" {
   
 }
 
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "my_eks_cluster_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "attach_s3_access_policy" {
   role       = aws_iam_role.eks_cluster_role.name
   policy_arn = aws_iam_policy.s3_access_policy.arn
